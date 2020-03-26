0355c3f28c44f94de48f0d371e125fdfb8b76fb6
a-stanisz <hello@adamstaniszewski.com>
2020-03-26 19:58:09 +0100

    React JSX intro

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function getTitle(title) {
+  return title;
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello {getTitle('React')}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
